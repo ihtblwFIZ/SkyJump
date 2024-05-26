d54bafd772a166981aeb639484713ed37f0f1ce2
hwangjaehee <zh_hui@naver.com>
2024-05-26 15:58:00 +0900

    feat : wallObject 주요 메서드 생성

---
+++ src/GameObject/WallObject.java
@@ -0,0 +1,22 @@
+package GameObject;
+
+import java.awt.*;
+
+public class WallObject extends GameObject {
+    /*
+    x,y : 좌상단 모서리 좌표
+    w : 벽의 가로 길이(너비)
+    h : 벽의 세로 길이(높이)
+     */
+    public WallObject(int x,int y, int w, int h){
+        super(x,y,w,h);
+        color= Color.orange;    //벽 내부 채울 색 설정
+        borderColors=Color.DARK_GRAY;   //벽 테두리 색 설정
+    }
+    public void draw(Graphics g){
+        g.setColor(color);
+        g.fillRect(getX(),getY(),getW(),getH());
+        g.setColor(borderColors);
+        g.drawRect(getX(),getY(),getW(),getH());
+    }
+}
