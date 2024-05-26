5955e56d4b179af502f81f184427f3f722224444
ihtblwFIZ <vthyrachel@naver.com>
2024-05-26 23:03:08 +0900

    feat: Monster 클래스 추가, GameObject 세미콜론 삭제

---
+++ src/GameObject/Monster.java
@@ -0,0 +1,15 @@
+package GameObject;
+
+import java.awt.*;
+
+public class Monster extends GameObject{
+    public Monster(int x, int y, int w, int h) {
+        super(x, y, w, h);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+}
