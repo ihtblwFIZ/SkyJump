f0cb16a70b6f906c2d3b97e2366c6e48257c81a0
Dldusdn1 <detol123@naver.com>
2024-06-08 21:38:14 +0900

    feat:Background Panel 생성 및 구현

---
+++ src/GamePanel/BackgroudPanel.java
@@ -0,0 +1,28 @@
+package GamePanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+public class BackgroudPanel extends JPanel {
+
+    boolean running = false;
+
+    int width = 0, height = 0;
+    static int highScore = 0;
+    static int yourScore = 0;
+    public BackgroudPanel(int w, int h) {
+        setBackground(new Color(220, 240, 255));
+        width = w;
+        height = h;
+    }
+
+    public void treadState(boolean state) {
+        running = state;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+    }
+}
